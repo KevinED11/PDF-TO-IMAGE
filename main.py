4dbe9c0ba043754c1838a5f1eb37d7f8f8cd031d
KevinED11 <mendez_kael@hotmail.com>
2024-08-13 01:14:49 -0700

    3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pdf2image import convert_from_path
 from enum import StrEnum, IntEnum
 from pathlib import Path
+from PIL import Image
 
 
 class Format(StrEnum):
@@ -14,7 +15,10 @@ class DPIQuality(IntEnum):
 
 
 # Ruta del archivo PDF
-pdf_path = Path("/home/kevind/Documentos/datos-personales/firma-autografa-digit.pdf")
+pdf_path = Path(
+    "/home/kevind/Documentos/datos-personales/firmas_autografas/firma-autografa-digit.pdf"
+)
+
 # Opciones para alta calidad
 DPI = 300  # Resolución en puntos por pulgada
 
@@ -25,3 +29,31 @@ images = convert_from_path(pdf_path=str(pdf_path), dpi=DPIQuality.DPI_300)
 for i, image in enumerate(images):
     image.save(f"pagina_{i + 1}.{Format.PNG}", format=Format.PNG)
 
+new_image = Image.open("pagina_1.PNG").convert("RGBA")
+data_image = new_image.getdata()
+
+
+class Channel(IntEnum):
+    R = 0
+    G = 1
+    B = 2
+    A = 3
+
+
+TRANSPARENT_PIXEL = (255, 255, 255, 0)
+new_data_image = []
+
+for pixel in data_image:
+    if (
+        pixel[Channel.R] > 200
+        and pixel[Channel.G] > 200
+        and pixel[Channel.B] > 200
+        and pixel[Channel.A] > 0
+    ):
+        new_data_image += [TRANSPARENT_PIXEL]
+        continue
+
+    new_data_image += [pixel]
+
+new_image.putdata(new_data_image)
+new_image.save("pagina_1_transparent.PNG", format=Format.PNG)
